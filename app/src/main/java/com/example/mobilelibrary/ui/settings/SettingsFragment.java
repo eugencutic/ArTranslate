3f156d04d5ad8599a7da7983b73dfc70d515cd09
Eugen Cutic <eugen_cutic@yahoo.com>
2020-03-16 13:14:44 +0200

    add settings fragment

---
+++ app/src/main/java/com/example/mobilelibrary/ui/settings/SettingsFragment.java
@@ -0,0 +1,42 @@
+package com.example.mobilelibrary.ui.settings;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mobilelibrary.R;
+
+public class SettingsFragment extends Fragment {
+
+    private SettingsViewModel settingsViewModel;
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        final TextView textView = root.findViewById(R.id.text_settings);
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+}
